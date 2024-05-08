315ea27f861cf92dc53d2f49bfc5139cf3a800cf
RadIbn Ahmed <radahmed62@gmail.com>
2024-05-08 10:58:43 +0000

    mostra immagine

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
+import pandas as pd
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('amazom.html')
+    return render_template('amazon.html', part='part1')
 
 @app.route('/elenco')
-def home():
-    return render_template('amazom.html')
+def elenco():
+    df = pd.read_csv('data/dati.csv')  
+    return render_template('amazon.html', tabella=df.to_html(), part='part2', prodotti=df['immagini'].tolist(),nome=df['nome'].tolist(),prezzo=df['prezzo'].tolist(),quantità=df['quantità'].tolist(),categoria=df['categoria'].tolist())
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+    app.run(host='0.0.0.0', port=3245, debug=True)
